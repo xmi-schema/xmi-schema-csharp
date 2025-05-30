c448a191c58243813c93e0a39257e6395a1ac5f4
HETANG404 <hetang401@gmail.com>
2025-05-30 09:33:13 +0800

    new structure

--- Models/XmiModel/XmiModel.cs
+++ Models/XmiModel/XmiModel.cs
@@ -1,12 +1,87 @@
 using XmiSchema.Core.Entities;
 using XmiSchema.Core.Relationships;
+using XmiSchema.Core.Geometries;
 
-
-namespace XmiSchema.Core.Models;
-
+namespace XmiSchema.Core.Models
+{
     public class XmiModel
     {
         public List<XmiBaseEntity> Entities { get; set; } = new();
         public List<XmiBaseRelationship> Relationships { get; set; } = new();
-    }
 
+        // 添加不同类型的实体
+        public void AddXmiStructuralMaterial(XmiStructuralMaterial material)
+        {
+            Entities.Add(material);
+        }
+
+        public void AddXmiStructuralCrossSection(XmiStructuralCrossSection crossSection)
+        {
+            Entities.Add(crossSection);
+        }
+
+        public void AddXmiStructuralCurveMember(XmiStructuralCurveMember member)
+        {
+            Entities.Add(member);
+        }
+
+        public void AddXmiStructuralSurfaceMember(XmiStructuralSurfaceMember member)
+        {
+            Entities.Add(member);
+        }
+
+        public void AddXmiStructuralPointConnection(XmiStructuralPointConnection connection)
+        {
+            Entities.Add(connection);
+        }
+
+        public void AddXmiStructuralStorey(XmiStructuralStorey storey)
+        {
+            Entities.Add(storey);
+        }
+
+        public void AddXmiPoint3D(XmiPoint3D point)
+        {
+            Entities.Add(point);
+        }
+
+        // 添加不同类型的关系
+        public void AddXmiHasPoint3D(XmiHasPoint3D relation)
+        {
+            Relationships.Add(relation);
+        }
+
+        public void AddXmiHasStructuralMaterial(XmiHasStructuralMaterial relation)
+        {
+            Relationships.Add(relation);
+        }
+
+        public void AddXmiHasStructuralNode(XmiHasStructuralNode relation)
+        {
+            Relationships.Add(relation);
+        }
+
+        public void AddXmiHasStructuralCrossSection(XmiHasStructuralCrossSection relation)
+        {
+            Relationships.Add(relation);
+        }
+
+        public void AddXmiHasStorey(XmiHasStructuralStorey relation)
+        {
+            Relationships.Add(relation);
+        }
+
+        // 查询
+        public string GetMatchingPoint3DId(XmiPoint3D importedPoint)
+        {
+            return Entities
+                .OfType<XmiPoint3D>()
+                .FirstOrDefault(p => p.X == importedPoint.X && p.Y == importedPoint.Y && p.Z == importedPoint.Z)?.ID;
+        }
+
+        public List<T> GetEntitiesOfType<T>() where T : XmiBaseEntity
+        {
+            return Entities.OfType<T>().ToList();
+        }
+    }
+}
--- XmiManager.cs
+++ XmiManager.cs
@@ -6,303 +6,174 @@ using XmiSchema.Core.Relationships;
 using Newtonsoft.Json;
 using System.Reflection;
 
-
-namespace XmiSchema.Core.Manager;
-
-public class XmiManager : IXmiManager
+namespace XmiSchema.Core.Manager
 {
-    public List<XmiModel> Models { get; set; }
+    public class XmiManager : IXmiManager
+    {
+        public List<XmiModel> Models { get; set; } = new();
 
-    public XmiManager()
-    {
-        Models = new List<XmiModel>();
-    }
+        private bool IsValidModelIndex(int index) => index >= 0 && index < Models.Count;
 
-    // 添加一个实体到指定索引的模型
-    public void AddXmiStructuralMaterialToModel(int modelIndex, XmiStructuralMaterial xmiStructuralMaterial)
-    {
-        if (IsValidModelIndex(modelIndex))
+        // ========== 添加实体 ==========
+        public void AddXmiStructuralMaterialToModel(int modelIndex, XmiStructuralMaterial material)
         {
-            Models[modelIndex].Entities.Add(xmiStructuralMaterial);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-    public void AddXmiStructuralCrossSectionToModel(int modelIndex, XmiStructuralCrossSection xmiStructuralCrossSection)
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            Models[modelIndex].Entities.Add(xmiStructuralCrossSection);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-    public void AddXmiStructuralCurveMemberToModel(int modelIndex, XmiStructuralCurveMember xmiStructuralCurveMember)
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            Models[modelIndex].Entities.Add(xmiStructuralCurveMember);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-    public void AddXmiStructuralPointConnectionToModel(int modelIndex, XmiStructuralPointConnection xmiStructuralPointConnection)
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            Models[modelIndex].Entities.Add(xmiStructuralPointConnection);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-    public void AddXmiStructuralStoreyToModel(int modelIndex, XmiStructuralStorey xmiStructuralStorey)
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            Models[modelIndex].Entities.Add(xmiStructuralStorey);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-    public void AddXmiPoint3DToModel(int modelIndex, XmiPoint3D xmiPoint3D)
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            Models[modelIndex].Entities.Add(xmiPoint3D);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-    public void AddXmiStructuralSurfaceMemberToModel(int modelIndex, XmiStructuralSurfaceMember xmiStructuralSurfaceMember)
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            Models[modelIndex].Entities.Add(xmiStructuralSurfaceMember);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-
-
-
-    // 添加多个实体到指定索引的模型
-    public void AddEntitiesToModel(int modelIndex, List<XmiBaseEntity> entities)
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            Models[modelIndex].Entities.AddRange(entities);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-
-
-    // 查询实体从指定索引的模型
-    public string GetMatchingPoint3DId(int modelIndex, XmiPoint3D importedPoint)
-    {
-        if (!IsValidModelIndex(modelIndex))
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiStructuralMaterial(material);
         }
 
-        foreach (var entity in Models[modelIndex].Entities)
+        public void AddXmiStructuralCrossSectionToModel(int modelIndex, XmiStructuralCrossSection crossSection)
         {
-            if (entity is XmiPoint3D point)
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiStructuralCrossSection(crossSection);
+        }
+
+        public void AddXmiStructuralCurveMemberToModel(int modelIndex, XmiStructuralCurveMember member)
+        {
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiStructuralCurveMember(member);
+        }
+
+        public void AddXmiStructuralSurfaceMemberToModel(int modelIndex, XmiStructuralSurfaceMember member)
+        {
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiStructuralSurfaceMember(member);
+        }
+
+        public void AddXmiStructuralPointConnectionToModel(int modelIndex, XmiStructuralPointConnection connection)
+        {
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiStructuralPointConnection(connection);
+        }
+
+        public void AddXmiStructuralStoreyToModel(int modelIndex, XmiStructuralStorey storey)
+        {
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiStructuralStorey(storey);
+        }
+
+        public void AddXmiPoint3DToModel(int modelIndex, XmiPoint3D point)
+        {
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiPoint3D(point);
+        }
+
+        // 批量添加
+        public void AddEntitiesToModel(int modelIndex, List<XmiBaseEntity> entities)
+        {
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            foreach (var e in entities)
             {
-                if (point.X == importedPoint.X &&
-                    point.Y == importedPoint.Y &&
-                    point.Z == importedPoint.Z)
-                {
-                    return point.ID;
-                }
+                Models[modelIndex].Entities.Add(e);
             }
         }
 
-        return null; // 或者也可以 return "-1"; 根据业务需要
-    }
-
-
-
-    // 构建关系
-
-
-    // 添加一个关系到指定索引的模型
-    public void AddXmiHasPoint3DToModel(int modelIndex, XmiHasPoint3D xmiHasPoint3D)
-    {
-        if (IsValidModelIndex(modelIndex))
+        // ========== 添加关系 ==========
+        public void AddXmiHasPoint3DToModel(int modelIndex, XmiHasPoint3D relation)
         {
-            Models[modelIndex].Relationships.Add(xmiHasPoint3D);
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiHasPoint3D(relation);
         }
-        else
+
+        public void AddXmiHasStructuralMaterialToModel(int modelIndex, XmiHasStructuralMaterial relation)
         {
-            throw new IndexOutOfRangeException("模型索引无效");
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiHasStructuralMaterial(relation);
         }
-    }
-    public void AddXmiHasStructuralMaterialToModel(int modelIndex, XmiHasStructuralMaterial xmiHasStructuralMaterial)
-    {
-        if (IsValidModelIndex(modelIndex))
+
+        public void AddXmiHasStructuralNodeToModel(int modelIndex, XmiHasStructuralNode relation)
         {
-            Models[modelIndex].Relationships.Add(xmiHasStructuralMaterial);
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiHasStructuralNode(relation);
         }
-        else
+
+        public void AddXmiHasStructuralCrossSectionToModel(int modelIndex, XmiHasStructuralCrossSection relation)
         {
-            throw new IndexOutOfRangeException("模型索引无效");
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiHasStructuralCrossSection(relation);
         }
-    }
 
-    public void AddXmiHasStructuralNodeToModel(int modelIndex, XmiHasStructuralNode xmiHasStructuralNode)
-    {
-        if (IsValidModelIndex(modelIndex))
+        public void AddXmiHasStoreyToModel(int modelIndex, XmiHasStructuralStorey relation)
         {
-            Models[modelIndex].Relationships.Add(xmiHasStructuralNode);
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            Models[modelIndex].AddXmiHasStorey(relation);
         }
-        else
+
+        // ========== 查询 ==========
+        public string GetMatchingPoint3DId(int modelIndex, XmiPoint3D importedPoint)
         {
-            throw new IndexOutOfRangeException("模型索引无效");
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            return Models[modelIndex].GetMatchingPoint3DId(importedPoint);
         }
-    }
 
-    public void AddXmiHasStructuralCrossSectionToModel(int modelIndex, XmiHasStructuralCrossSection xmiHasStructuralCrossSection)
-    {
-        if (IsValidModelIndex(modelIndex))
+        public List<T> GetEntitiesOfType<T>(int modelIndex) where T : XmiBaseEntity
         {
-            Models[modelIndex].Relationships.Add(xmiHasStructuralCrossSection);
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+            return Models[modelIndex].GetEntitiesOfType<T>();
         }
-        else
+
+        // ========== 构建与保存 ==========
+        public string BuildJson(int modelIndex)
         {
-            throw new IndexOutOfRangeException("模型索引无效");
+            if (!IsValidModelIndex(modelIndex)) throw new IndexOutOfRangeException();
+
+            var model = Models[modelIndex];
+            var nodes = model.Entities.Select(GetAttributes).ToList();
+            var edges = model.Relationships.Select(GetAttributes).ToList();
+
+            var graphJson = new { nodes, edges };
+
+            return JsonConvert.SerializeObject(graphJson, Formatting.Indented);
         }
-    }
 
-    public void AddXmiHasStoreyToModel(int modelIndex, XmiHasStructuralStorey xmiHasStructuralStorey)
-    {
-        if (IsValidModelIndex(modelIndex))
+        private Dictionary<string, object> GetAttributes(object obj)
         {
-            Models[modelIndex].Relationships.Add(xmiHasStructuralStorey);
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
+            var dict = new Dictionary<string, object>();
+            var props = obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                           .OrderBy(p => p.MetadataToken);
 
-    
-    // 泛型查询
-
-    public List<T> GetEntitiesOfType<T>(int modelIndex) where T : XmiBaseEntity
-    {
-        if (IsValidModelIndex(modelIndex))
-        {
-            return Models[modelIndex].Entities
-                .OfType<T>()
-                .ToList();
-        }
-        else
-        {
-            throw new IndexOutOfRangeException("模型索引无效");
-        }
-    }
-
-    // 私有辅助方法：验证索引是否在范围内
-    private bool IsValidModelIndex(int index)
-    {
-        return index >= 0 && index < Models.Count;
-    }
-
-
-
-    public string BuildJson(int modelIndex)
-    {
-        var nodes = Models[modelIndex].Entities
-            .Select(e => GetAttributes(e))
-            .ToList();
-
-        var edges = Models[modelIndex].Relationships
-            .Select(r => GetAttributes(r))
-            .ToList();
-
-        var graphJson = new
-        {
-            nodes,
-            edges
-        };
-
-        return JsonConvert.SerializeObject(graphJson, Formatting.Indented);
-    }
-
-    private Dictionary<string, object> GetAttributes(object obj)
-    {
-        var dict = new Dictionary<string, object>();
-
-        var props = obj.GetType()
-            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
-            .OrderBy(p => p.MetadataToken); // 按声明顺序输出
-
-        foreach (var prop in props)
-        {
-            var value = prop.GetValue(obj);
-            if (value == null) continue;
-
-            var type = prop.PropertyType;
-            object finalValue = null;
-
-            if (type.IsEnum)
+            foreach (var prop in props)
             {
-                var enumName = value.ToString();
-                if (!string.IsNullOrEmpty(enumName))
+                var value = prop.GetValue(obj);
+                if (value == null) continue;
+
+                var type = prop.PropertyType;
+                object finalValue = null;
+
+                if (type.IsEnum)
                 {
+                    var enumName = value.ToString();
                     var field = type.GetField(enumName);
                     var enumValueAttr = field?.GetCustomAttribute<EnumValueAttribute>();
                     finalValue = enumValueAttr?.Value ?? enumName;
                 }
-                else
+                else if (type.IsPrimitive || type == typeof(string) ||
+                         type == typeof(decimal) || type == typeof(DateTime) ||
+                         type == typeof(float) || type == typeof(double))
                 {
                     finalValue = value;
                 }
-            }
-            else if (type.IsPrimitive || type == typeof(string) ||
-                     type == typeof(decimal) || type == typeof(DateTime) ||
-                     type == typeof(float) || type == typeof(double))
-            {
-                finalValue = value;
-            }
-            else if (value is XmiBaseEntity entityRef)
-            {
-                finalValue = entityRef.ID;
-            }
-            else if (value is IEnumerable<XmiBaseEntity> entityList)
-            {
-                finalValue = entityList.Select(e => e.ID).ToList();
+                else if (value is XmiBaseEntity entityRef)
+                {
+                    finalValue = entityRef.ID;
+                }
+                else if (value is IEnumerable<XmiBaseEntity> entityList)
+                {
+                    finalValue = entityList.Select(e => e.ID).ToList();
+                }
+
+                if (finalValue != null)
+                {
+                    dict[prop.Name] = finalValue;
+                }
             }
 
-            if (finalValue != null)
-            {
-                dict[prop.Name] = finalValue;
-            }
+            return dict;
         }
-        // return
-        return dict;
-    }
-    public void Save(string path)
-    {
-        // 目前默认为0
-        File.WriteAllText(path, BuildJson(0));
-        Console.WriteLine($"JSON 图文件保存成功：{path}");
+
+        public void Save(string path)
+        {
+            File.WriteAllText(path, BuildJson(0));
+            Console.WriteLine($"JSON 图文件保存成功：{path}");
+        }
     }
 }
